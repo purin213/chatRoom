d8960c1008466f3cc5432fd2817381a61ab74eed
Christian <purin213@hotmail.com>
2023-09-14 22:11:07 +0900

    add recieve

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ import socket
 import threading
 
 host = '127.0.0.1'
-
 port = 8080
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -35,4 +34,17 @@ def handle(client):
 
 def receive():
     while True:
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+        client.send('NICKNAME'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
+        thread = threading.Thread(target=handle, arges=(client,))
+        thread.start()
 
+
+receive()
