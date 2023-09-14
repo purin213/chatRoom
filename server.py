530eeff9e956216efee12fa761d4597fae36ea30
Christian <purin213@hotmail.com>
2023-09-14 22:05:42 +0900

    add client functions

--- server.py
+++ server.py
@@ -8,3 +8,31 @@ port = 8080
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
+
+clients = []
+nicknames = []
+
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast('{} left!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+
+def receive():
+    while True:
+
